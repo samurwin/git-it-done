e4545c6a35a0176e45c8a86ee6423b5f843b54bd
smudesign <urwinsamantha@gmail.com>
2021-09-22 12:44:59 -0400

    list issues and get more

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,13 +1,18 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiurl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiurl).then(function(response) {
         //request was successful
         if (response.ok) {
-            response.json()
-            .then(function(data) {
+            response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayIssues(repo);
+                }
             })
         } else {
             alert("There was a problem with your request!");
@@ -53,4 +58,16 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("microsoft/terminal");
